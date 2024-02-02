a578bfbb2b0d355fbbea138c56cb4d78a71d9e86
Aditya Raj <adi.adityakohli2005@gmail.com>
2024-02-02 15:53:53 +0530

    hour correction

--- script.js
+++ script.js
@@ -9,15 +9,15 @@ window.addEventListener('load', () => {
         element.style.transform = `rotate(${index * 6}deg)`;
         beneth.appendChild(element);
     }
-    
-    setInterval(()=>{
+
+    setInterval(() => {
         let time = new Date();
         let second_time = time.getSeconds();
         let minute_time = time.getMinutes();
-        let hour_time = time.getHours()%12;
-        second.style.transform = `rotate(${second_time*6-90}deg)`;
-        minute.style.transform = `rotate(${minute_time*6-90+6*second_time/60}deg)`;
-        hour.style.transform = `rotate(${hour_time*6-90+6*minute_time/60}deg)`;
+        let hour_time = time.getHours() % 12;
+        second.style.transform = `rotate(${second_time * 6 - 90}deg)`;
+        minute.style.transform = `rotate(${minute_time * 6 - 90 + 6 * second_time / 60}deg)`;
+        hour.style.transform = `rotate(${hour_time * 30 - 90 + 0.5 * minute_time }deg)`;
         console.log(minute_time)
     }, 1000);
 });
